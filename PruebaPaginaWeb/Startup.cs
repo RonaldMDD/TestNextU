94d319314ad3c5a435dfeb7e6ae80638c6b56e33
RonaldMDD <RonaldMDD@hotmail.com>
2017-12-10 12:52:33 -0500

    Initial draft

---
+++ PruebaPaginaWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PruebaPaginaWeb.Startup))]
+namespace PruebaPaginaWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
